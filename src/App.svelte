ce87792f072fb6f8e2e9f74396902e2a8f4b11b4
Mario Nachbaur <marionauta@users.noreply.github.com>
2021-02-01 20:11:50 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,79 @@
+<script>
+	const ratio = 166.386;
+
+	let euros = 0;
+	let pesetas = 0;
+
+	function onChangeEuros() {
+	  pesetas = (euros * ratio).toFixed(2);
+	}
+
+	function onChangePesetas() {
+	  euros = (pesetas / ratio).toFixed(2);
+	}
+</script>
+
+<style>
+	.app {
+	  margin: 1rem;
+	  display: flex;
+	  align-items: center;
+	  justify-content: center;
+	  height: 90vh;
+	  font: 40px sans-serif;
+	}
+
+	.container {
+	  display: grid;
+	  grid-template-columns: 2fr 3fr;
+	  align-items: center;
+	  grid-gap: 1rem;
+	  width: 100%;
+	  max-width: 800px;
+	}
+
+	.currency-title {
+	  text-align: right;
+	}
+
+	.currency-input {
+	  font: inherit;
+	  padding: 0.5rem;
+	  width: 100%;
+	  box-sizing: border-box;
+	}
+
+	@media (max-width: 600px) {
+	  .container {
+	    grid-template-columns: 1fr;
+	  }
+
+	  .currency-title {
+	    text-align: center;
+	  }
+	}
+</style>
+
+<div class=app>
+	<div class=container>
+		<span class=currency-title>
+			Euros
+		</span>
+		<input
+			class=currency-input
+			type=number
+			bind:value={euros}
+			on:input={onChangeEuros}
+		>
+
+		<span class=currency-title>
+			Pesetas
+		</span>
+		<input
+			class=currency-input
+			type=number
+			bind:value={pesetas}
+			on:input={onChangePesetas}
+		>
+	</div> <!-- .container -->
+</div> <!-- .app -->
